5c914c7d604dc267fb78b2c961570406f383f444
Ben <Dweck@BenMBP16.local>
2020-01-01 15:43:52 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const myFunc = () => {
+    if (false) {
+      return 'foo';
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
